9e80a9e9b5864ace03ef90a4b848486a89d0f42e
Mohammad Eftekhari <bluemmb22@gmail.com>
2023-09-30 10:51:24 +0200

    Init project

---
+++ src/main/java/com/me/frankanne/FrankanneApplication.java
@@ -0,0 +1,13 @@
+package com.me.frankanne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankanneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankanneApplication.class, args);
+	}
+
+}
